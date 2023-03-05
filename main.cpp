d246886f956063f1d26c394d0396e0a4d18ec683
Minh Nhat <vothanhminhnhat@gmail.com>
2023-03-06 06:47:47 +0700

    seconds

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ git init
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/truonghuynhquangthong/C-C-exercise.git
-git push -u origin main
+
 */
 #include <iostream>
 #include <stdarg.h>
